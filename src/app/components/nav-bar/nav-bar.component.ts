b3ccf2b91f7d6e948e2982ff38d6ebe650bb1d54
rafaelcesar0 <rafaelcesar0@proton.me>
2024-08-19 10:06:19 -0300

    fix: update nav-bar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 import { MenuItem, PrimeIcons } from 'primeng/api';
 
 @Component({
@@ -10,34 +9,18 @@ import { MenuItem, PrimeIcons } from 'primeng/api';
 export class NavBarComponent {
   items: MenuItem[] = [];
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
-
   ngOnInit() {
     this.items = [
       {
         label: 'Apartamentos',
         icon: PrimeIcons.BUILDING,
         routerLink: '/apartamentos',
-        command: () => this.updateActiveItem('/apartamentos'),
       },
       {
         label: 'Veiculos',
         icon: PrimeIcons.CAR,
         routerLink: '/veiculos',
-        command: () => this.updateActiveItem('/veiculos'),
       },
     ];
-
-    this.updateActiveItem(this.router.url);
-  }
-
-  updateActiveItem(url: string) {
-    this.items.forEach((item) => {
-      if (item.routerLink === url) {
-        item.styleClass = 'active-menu-item';
-      } else {
-        item.styleClass = '';
-      }
-    });
   }
 }
